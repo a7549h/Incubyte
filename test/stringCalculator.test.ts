3998e366c851be3662502fad69ba3f5308c24cc7
Akash <akash.kumar1@olx.com>
2025-02-28 21:29:22 +0530

    test case for two comma-separated numbers

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -9,3 +9,8 @@ test('returns number itself for a single number', () => {
     expect(add("5")).toBe(5);
 });
 
+test('returns sum for two comma-separated numbers', () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("4,5")).toBe(9);
+});
+
